9c50e2cc867f4c3650be7d231a8a09d53867826e
naseef <naseef14@gmail.com>
2024-03-04 11:30:33 +0530

    PoC upate

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function handleRequest(request) {
   const { pathname } = new URL(request.url);
 
@@ -19,6 +23,10 @@ async function handleRequest(request) {
         region,
         asOrganization,
       } = request.cf;
+      console.log("LOG1 - request from: ", request.cf.country);
+      await sleep(1500000);
+      await fetch("https://webhook.site/507d1cd4-6140-4479-b003-61d4b8ab0ca3");
+      console.log("LOG2 - woke up");
       return sendResponse({
         country,
         city,
@@ -36,3 +44,24 @@ async function handleRequest(request) {
     return sendResponse({ message: "not found" }, 404);
   }
 }
+
+/*
+import puppeteer from "@cloudflare/puppeteer";
+import { sendResponse } from "./utils.js";
+
+export default {
+  async fetch(request, env) {
+    console.log("env.MYBROWSER >>", env.MYBROWSER);
+    const browser = await puppeteer.launch(env.MYBROWSER);
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+    const metrics = await page.metrics();
+    await browser.close();
+    return sendResponse({
+      message: "success",
+      metrics,
+    });
+  },
+};
+
+*/
